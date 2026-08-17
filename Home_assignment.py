e62a614e4a56aa9acf6d98a73d7550e5a131f113
vedikchittem@gmail.co.uk <vedikchittem@gmail.co.uk>
2026-08-17 10:49:15 +0100

    hello

---
+++ Home_assignment.py
@@ -0,0 +1,7 @@
+#make the photo ready for a party
+#imagine your image is going to win a photo party you have to perform two operations 
+#task 
+#task 1 is to read an image using opencv
+#rotate the image by 90 degrees
+#increase its brightness by thirty
+#display the final image with the title party ready image
--- Image_processing.py
+++ Image_processing.py
@@ -5,7 +5,18 @@ import matplotlib.pyplot as plt
 image = cv2.imread("family.jpeg")
 print(image.shape)
 
-rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-(h, w) = image.shape[:2]
-plt.imshow(rgb_image)
+rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+(h , w ) = image.shape[:2]
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center,180,1.0)
+rotated = cv2.warpAffine(image,M,(w,h))
+rotated_rgb = cv2.cvtColor(rotated,cv2.COLOR_BGR2RGB)
+plt.imshow(rotated_rgb)
+plt.show()
+# increase brightness
+brightness_metrix = np.ones(image.shape, dtype = "uint 8") * 50
+# add brightness
+brighter = cv2.add(image,brightness_metrix)
+brighter_rgb = cv2.cvtColor(brighter, cv2.COLOR_BGR2RGB)
+plt.imshow(brighter_rgb)
 plt.show()
