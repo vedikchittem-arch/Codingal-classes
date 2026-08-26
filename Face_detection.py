e26bc386f68e276878ed709ad673f1ad9f113298
vedikchittem@gmail.co.uk <vedikchittem@gmail.co.uk>
2026-08-26 13:31:46 +0100

    test

--- Face_detection.py
+++ Face_detection.py
@@ -1,8 +1,12 @@
 import cv2
+print(cv2.data.haarcascades)
 print(cv2.__file__)
 print(cv2.__version__)
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascades_frontalface_default.xml")
-cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+print("Camera opened successfully", cap.isOpened())
 
 if not cap.isOpened():
     print("Error: Could not open the camera")
@@ -10,6 +14,7 @@ if not cap.isOpened():
 
 while True:
     ret, frame = cap.read()
+    print("Frame captured successfully", ret)
     if not ret:
         print("Error: Failed to capture image")
         break
@@ -19,5 +24,13 @@ while True:
                                           minNeighbors = 5,
                                           minSize = (30, 30)  
                                           )
+    for (x, y, w, h) in faces: 
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow("Face Detection", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
     
