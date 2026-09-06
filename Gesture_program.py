16a8fc2795e9e0e90925a441981dd884e96ccbc7
vedikchittem@gmail.co.uk <vedikchittem@gmail.co.uk>
2026-09-06 15:44:33 +0100

    hand

--- Gesture_program.py
+++ Gesture_program.py
@@ -23,5 +23,28 @@ while True:
     result = cv2.bitwise_and(frame, frame, mask = mask)
 
     contours,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        max_countours = max(contours, key=cv2.contourArea)
+    if cv2.contourArea(max_countours) > 500:
+        x,y,w,h = cv2.boundingRect(max_countours)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        center_x = int(x+w / 2)
+        center_y = int(y+h / 2)
+        cv2.circle(frame,(center_x,center_y),5,(0,0,255),-1)
+        
+        message = "hello👋"
+        cv2.putText(frame,message,(x,y-10),
+                    cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+    cv2.imshow("Original frame", frame)
+    cv2.imshow("Filtered frmae", result)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
+
+
+
 
     
--- party.py
+++ party.py
@@ -1,68 +1,3 @@
-#Create a program that opens the webcam and shows a funny message based on the number of detected faces
-# for zero face where is everybody
-# one face say hey boss 😎
-# two faces say Party has started 🥳🥳🎉
-# 
-
-import cv2
-
-from PIL import Image, ImageDraw, ImageFont
-
-# Open webcam
-cap = cv2.VideoCapture(0)
-
-# Load face detector
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
-font_path = "C:/Windows/Fonts/seguiemj.ttf"
-font = ImageFont.truetype(font_path, 32)
-
-while True:
-    ret, frame = cap.read()
-
-    if not ret:
-        break
-    pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    draw = ImageDraw.Draw(pil_image)
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # Number of faces
-    face_count = len(faces)
-
-    # Choose message
-    if face_count == 0:
-        message = "Where is everybody?"
-    elif face_count == 1:
-        message = "Hey boss 😎"
-    elif face_count == 2:
-        message = "Party has started 🥳🥳🎉"
-    else:
-        message = f"Wow! {face_count} people here! 🎉"
-
-    # Display message
-    draw.text((20, 20), message, font= font, fill= (255, 255, 0))
-    frame = cv2.cvtColor(pil_image, cv2.COLOR_RGB2BGR)
-
-    
-    cv2.imshow("Funny Face Detector", frame)
-
-    
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-# Close everything
-cap.release()
-cv2.destroyAllWindows()
-
-
-
 import cv2
 import numpy as np
 
@@ -74,15 +9,15 @@ if not cap.isOpened():
 
 mode = "NORMAL"
 
-#print("""
-#Camera Controls
-#---------------
-#R = Red Mode 🍎
-#G = Green Alien Mode 👽
-#C = Canny X-Ray Mode 🩻
-#T = Cartoon Mode 📺
-#Q = Quit 🚪
-#""")
+print("""
+Camera Controls
+---------------
+R = Red Mode 🍎
+G = Green Alien Mode 👽
+C = Canny X-Ray Mode 🩻
+T = Cartoon Mode 📺
+Q = Quit 🚪
+""")
 
 while True:
     ret, frame = cap.read()
@@ -204,7 +139,4 @@ while True:
         break
 
 cap.release()
-cv2.destroyAllWindows()           
-
-
-
+cv2.destroyAllWindows()  
